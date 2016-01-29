7ea84b091579e3c7937cfa4b156fa98d320858cb
Shane Osbourne <shane.osbourne8@gmail.com>
2016-01-29 21:33:00 +0000

    ...

--- test/command.watch.init.js
+++ test/command.watch.init.js
@@ -1,34 +1,36 @@
-//var assert = require('chai').assert;
-//var watch  = require('../lib/command.watch');
-//var cli    = require('../');
-//
-//describe('Gathering watch tasks', function () {
-//    it('can gather tasks in shorthand format', function (done) {
-//        const runner = cli({
-//            input: ['watch'],
-//            flags: {
-//                handoff: true
-//            }
-//        }, {
-//            crossbow: {
-//                watch: {
-//                    tasks: {
-//                        default: {
-//                            "*.css": ["sass", "js"],
-//                            "*.js": "js"
-//                        }
-//                    }
-//                }
-//            }
-//        });
-//
-//        console.log(runner);
-//
-//        done();
-//        //assert.equal(tasks.default.watchers.length, 2);
-//        //assert.equal(tasks.default.watchers[0].patterns.length, 1);
-//        //assert.equal(tasks.default.watchers[0].patterns[0], "*.css");
-//        //assert.equal(tasks.default.watchers[0].tasks[0], "sass");
-//        //assert.equal(tasks.default.watchers[0].tasks[1], "js");
-//    });
-//});
+var assert = require('chai').assert;
+var watch  = require('../lib/command.watch');
+var cli    = require('../');
+
+describe('Initialising runner with task data', function () {
+    it.only('can gather all pre-tasks', function (done) {
+        const runner = cli({
+            input: ['watch', 'shane'],
+            flags: {
+                handoff: true
+            }
+        }, {
+            crossbow: {
+                watch: {
+                    tasks: {
+                        default: {
+                            "*.css": ["sass", "js"],
+                            "*.js": "js"
+                        }
+                    }
+                }
+            }
+        });
+
+        console.log(runner.tasks.default.watchers);
+        console.log(runner.watcherTasks);
+        console.log(runner.beforeTasks);
+
+        done();
+        //assert.equal(tasks.default.watchers.length, 2);
+        //assert.equal(tasks.default.watchers[0].patterns.length, 1);
+        //assert.equal(tasks.default.watchers[0].patterns[0], "*.css");
+        //assert.equal(tasks.default.watchers[0].tasks[0], "sass");
+        //assert.equal(tasks.default.watchers[0].tasks[1], "js");
+    });
+});
