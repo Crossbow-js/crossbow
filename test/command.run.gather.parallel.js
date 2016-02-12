59930f8d8388251c19e5d73833c42c592b5dfd12
Shane Osbourne <shane.osbourne8@gmail.com>
2016-02-12 14:22:27 +0000

    more work on fr

--- test/command.run.gather.parallel.js
+++ test/command.run.gather.parallel.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const cli = require("../");
+const Rx = require("rx");
 const createSeq = require("../dist/task.sequence").createSequence;
 
 function handoff(cmd, input, cb) {
@@ -12,8 +13,9 @@ function handoff(cmd, input, cb) {
 }
 
 describe('Gathering run tasks, grouped by runMode', function () {
-    it.only('can gather tasks when parallel syntax used', function () {
-        var runner = handoff(['build-all@p'], {
+    it.only('can gather tasks when parallel syntax used', function (done) {
+        this.timeout(10000);
+        var runner = handoff(['build-all'], {
             tasks: {
                 'build-all': ['js', 'css'],
                 'css':       ['@npm sass', '@npm postcss', '@shell ls', 'html'],
@@ -22,44 +24,124 @@ describe('Gathering run tasks, grouped by runMode', function () {
             }
         });
 
+
+
         function pullMany (items, parents) {
             return items.reduce(function (all, task) {
                 if (task.runMode === 'parallel' && task.tasks.length) {
+
                     return all.concat({
                         type: 'Parallel Group',
                         parents: parents,
-                        groups: task.tasks.map(task => {
-                            if (task.adaptor) {
-                                return {tasks: [task]};
+                        items: task.tasks.map(_task => {
+                            if (_task.adaptor) {
+                                return {
+                                    type: 'Item',
+                                    parents: parents.concat(task.taskName),
+                                    task: _task
+                                };
                             }
-                            if (task.tasks) {
+                            if (_task.tasks) {
                                 return {
                                     parents: parents,
-                                    tasks: pullMany(task.tasks, parents.concat(task.taskName))
+                                    tasks: pullMany(_task.tasks, parents.concat(task.taskName))
                                 };
                             }
                         })
                     });
-                } else {
-                    if (task.tasks.length) {
-                        return all.concat({
-                            type: 'Series Group', parents: parents, items: pullMany(task.tasks, parents.concat(task.taskName))
-                        });
-                    } else {
-                        return all.concat({
-                            type: 'Item',
-                            parents: parents,
-                            task: task
-                        });
-                    }
                 }
+
+                if (task.tasks.length) {
+                    return all.concat({
+                        type: 'Series Group',
+                        parents: parents,
+                        items: pullMany(task.tasks, parents.concat(task.taskName))
+                    });
+                }
+
+                return all.concat({
+                    type: 'Item',
+                    parents: parents,
+                    task: task
+                });
             }, []);
         }
-        
+
+        console.time('rx');
 
         const res = pullMany(runner.tasks.valid, []);
 
-        require('fs').writeFileSync('out.json', JSON.stringify(res, null, 4));
+        function getObs (items, initial) {
+            return items.reduce(function (acc, item) {
+
+                if (item.type === 'Series Group') {
+                    return acc.concat(Rx.Observable.concat(getObs(item.items, acc)));
+                }
+
+                if (item.type === 'Parallel Group') {
+
+                    return acc.concat(Rx.Observable.merge(getObs(item.items, acc)));
+                }
+
+                return acc.concat(createObservableForTask(item));
+
+            }, initial);
+        }
+
+        //log(res);
+        //done();
+        //console.log(getObs(res, [])[0]);
+
+        done();
+        //const obs = Rx.Observable.concat(getObs(res, [])).share();
+        //
+        //obs.subscribeOnNext(function (value) {
+        //    console.log(value);
+        //});
+        //
+        //obs.subscribeOnCompleted(function () {
+        //    console.timeEnd('rx');
+        //    done();
+        //});
+        ////
+        //
+        var now = new Date().getTime();
+        //
+        function createObservableForTask(item) {
+            return Rx.Observable.create(observer => {
+                setTimeout(function () {
+                    observer.onNext(item.task.taskName);
+                    observer.onCompleted();
+                }, 100);
+            }).timestamp()
+                .map(x => {
+                    x.timestamp = x.timestamp - now;
+                    return x;
+                })
+        }
+        //
+        //var s$ = Rx.Observable.merge(
+        //    createObservableForTask('1'),
+        //    createObservableForTask('2')
+        //);
+        //
+        //var s2$ = Rx.Observable.merge(
+        //    createObservableForTask('3'),
+        //    createObservableForTask('4')
+        //);
+        //
+        //const result$ = Rx.Observable.merge(s$, s2$);
+        //
+        //result$.subscribeOnCompleted(function () {
+        //    console.timeEnd('rx');
+        //    console.log('All done');
+        //    done();
+        //});
+
+        function log (obj) {
+            console.log(obj);
+            require('fs').writeFileSync('out.json', JSON.stringify(obj, null, 4));
+        }
 
         //console.log(runner.sequence[0]);
         //console.log(runner.sequence[1]);
