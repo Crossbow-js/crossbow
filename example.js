96cac1e2fc8154e036908bd3d46cb53338e80ecb
Shane Osbourne <shane.osbourne8@gmail.com>
2016-03-15 22:47:16 +0000

    Show example of nested tree disposal

--- example.js
+++ example.js
@@ -1,11 +1,48 @@
-var cb = require('./');
+'use strict';
 
-cb.runner(['lint', 'unit'])
-    //.series()
-    .parallel()
-    .do(x => console.log(x.type))
-    .where(x => x.type === 'end')
-    .toArray()
-    .subscribe(function (x) {
-        console.log(x);
+var rx = require('rx');
+var O = rx.Observable;
+
+function fac(name) {
+    return O.create(obs => {
+        console.log('running: ', name);
+        let i = setTimeout(function () {
+        	obs.onNext(name);
+        	obs.onCompleted();
+        }, 500);
+        return () => {
+            if (i) {
+                console.log('cleaning:', name);
+                clearTimeout(i);
+            }
+        }
     });
+}
+
+
+var array = [
+    fac(1),
+    fac(2),
+    O.concat(
+        fac(3),
+        fac(4),
+        O.merge(
+            fac(5),
+            fac(6)
+        )
+    )
+];
+
+const q = O.from(array).mergeAll();
+
+const sub = q.subscribe(x => {
+        console.log('value:   ', x)
+    }, e => {
+        console.error(e)
+    }, _ => {
+        console.log('DONE');
+    });
+
+setTimeout(function () {
+	sub.dispose();
+}, 600);
