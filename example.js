c9ec218541a6c55423570fb81d23f750d45a00e1
Shane Osbourne <shane.osbourne8@gmail.com>
2016-03-16 08:22:12 +0000

    Adding test exampls

--- example.js
+++ example.js
@@ -1,48 +1,84 @@
-'use strict';
+//'use strict';
+//
+var Rx = require('rx');
+var O = Rx.Observable;
 
-var rx = require('rx');
-var O = rx.Observable;
+function getTask(name, time, schedule) {
+    return O.create(observer => {
+        O.timer(time, schedule || null)
+            .subscribe(x => {
+                console.log('+', name);
+                observer.onNext(x);
+            }, e => {}, _ => {
+                observer.onCompleted();
+            });
+    })
+}
 
-function fac(name) {
-    return O.create(obs => {
-        console.log('running: ', name);
-        let i = setTimeout(function () {
-        	obs.onNext(name);
-        	obs.onCompleted();
-        }, 500);
-        return () => {
-            if (i) {
-                console.log('cleaning:', name);
-                clearTimeout(i);
-            }
-        }
+function getError(name, time, scheduler) {
+    return O.create(observer => {
+        O.timer(time, scheduler || null)
+            .subscribe(x => {
+                console.log('-', name);
+                observer.onError(new Error('Some proble'));
+            });
     });
 }
 
+//function fac(name) {
+//    return O.create(obs => {
+//        console.log('+ ', name);
+//        let i = setTimeout(function () {
+//        	obs.onNext(name);
+//        	obs.onCompleted();
+//        }, 500);
+//        return () => {
+//            if (i) {
+//                console.log('- ', name);
+//                clearTimeout(i);
+//            }
+//        }
+//    });
+//}
+const onNext  = Rx.ReactiveTest.onNext;
+const onError = Rx.ReactiveTest.onError;
 
-var array = [
-    fac(1),
-    fac(2),
-    O.concat(
-        fac(3),
-        fac(4),
-        O.merge(
-            fac(5),
-            fac(6)
+function getTasks (scheduler) {
+    return [
+        getTask('1', 100, scheduler),
+        getTask('2', 100, scheduler),
+        getError('3', 100, scheduler),
+        O.concat(
+            getTask('4', 100, scheduler),
+            getTask('5', 100, scheduler),
+            O.merge(
+                getTask('6', 100, scheduler),
+                getTask('7', 100, scheduler)
+            )
         )
-    )
-];
+    ];
+}
 
-const q = O.from(array).mergeAll();
+////const q = O.from(task1).concatAll();
+////
+////const sub = q.subscribe(x => {
+////        //console.log('+ ', x)
+////    }, e => {
+////        console.error(e)
+////    }, _ => {
+////        console.log('DONE');
+////    });
+////
+////setTimeout(function () {
+////	sub.dispose();
+////}, 600);
+//
+const TestScheduler = Rx.TestScheduler;
+const just          = O.just;
+const empty         = O.empty;
+const scheduler     = new TestScheduler();
 
-const sub = q.subscribe(x => {
-        console.log('value:   ', x)
-    }, e => {
-        console.error(e)
-    }, _ => {
-        console.log('DONE');
-    });
-
-setTimeout(function () {
-	sub.dispose();
-}, 600);
+var results = scheduler.startScheduler(function () {
+    return O.from(getTasks(scheduler)).concatAll();
+}, {created: 0, subscribed: 0, disposed: 5000});
+console.log(results.messages);
