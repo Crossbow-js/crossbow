1b6cb94abe1b967a8d195cac7aef73d0ad02a8e9
Shane Osbourne <shane.osbourne8@gmail.com>
2016-03-18 21:50:22 +0000

    Adding handleConcatStream method

--- example.js
+++ example.js
@@ -2,65 +2,160 @@
 //
 var Rx = require('rx');
 var O = Rx.Observable;
+var empty = O.empty;
+var concat = O.concat;
+var merge = O.merge;
 var cp = require('child_process').spawn;
+
+var subject = new Rx.ReplaySubject(2000);
+var sub$ = subject.share();
 
 function getcp (num, cmd, fail) {
     const obs = Rx.Observable.create(function (observer) {
         console.log(num, '+ running');
+        observer.onNext({type: 'start', id: num});
         var errored;
         var emit = cp('sh', ['-c', cmd], {stdio: 'inherit'});
 
         emit.on('close', function (code) {
             if (code === 0) {
                 console.log(num, '√ done');
+                observer.onNext({type: 'end', id: num});
                 observer.onCompleted();
             } else {
                 errored = true;
-                console.log(num, 'x errored');
+                console.log(num, 'x ERRORED');
+                observer.onNext({type: 'erorr', id: num, error: new Error('none-zero exit request'), exitCode: code});
                 observer.onError(new Error('none-zero exit request'));
             }
         }).on('error', function () {
             console.log('error');
         });
 
+        var single = new Rx.SingleAssignmentDisposable();
         var dis = Rx.Disposable.create(function () {
             if (typeof emit.exitCode !== "number") {
                 console.log(num, '- disposing cuz no exit code');
                 emit.removeAllListeners('close');
                 emit.on('close', function () {
                     console.log(num, '> disposed');
-                    dis.dispose();
+                    single.dispose();
                 });
                 emit.kill('SIGINT');
             } else {
-                console.log(num, 'EXIT CODE', emit.exitCode);
+                single.dispose();
             }
         });
-        return dis;
+        single.setDisposable(dis);
+        return single
     });
 
     return obs;
 }
 
-var stream = Rx.Observable.merge(
-    getcp(1, `sleep 1`),
-    getcp(2, 'sleep 1'),
+// handleMergeStream([
+//     getcp('-1', `sleep 1`).catch(e => O.empty()),
+//     getcp('-2', 'sleep 2').catch(e => O.empty()),
+//     O.merge(
+//         getcp('---3', 'sleep 1').catch(e => O.empty()),
+//         getcp('---4', 'sleep').catch(e => O.empty()),
+//         O.concat(getcp('---5', 'sleep 1'), getcp('------6', 'sleep 1'), getcp('---------7', 'sleep 1')).catch(e => O.empty()),
+//         getcp('---8', 'sleep 1').catch(e => O.empty())
+//     ).catch(e => O.empty()),
+//     O.concat(
+//         getcp('-9', 'sleep'),
+//         getcp('-10', 'sleep 1')
+//     ).catch(e => O.empty())
+// ]);
+
+/**
+ * Merge stream will never fail at the top level, so we need to capture
+ * output and look at it in the onCompleted callback
+ * @param items
+ */
+function handleMergeStream(items) {
+    Rx.Observable.merge(items)
+        .subscribe(x => {
+            subject.onNext(x);
+        }, e => {
+            // console.log('er');
+            // console.log('error', e);
+        }, _ => {
+            subject.forEach(function (x) {
+                console.log(x)
+            })
+        });
+}
+
+/**
+ * Concat stream can fail at any point, so we need to
+ * catch when that error occurs
+ * @param items
+ */
+function handleConcatStream(items) {
+    var subject = new Rx.ReplaySubject(2000);
+    Rx.Observable.from(items)
+        .concatAll()
+        .catch(e => { // global error
+            console.log('Error', 'but got these dope messages before it errored!');
+            report('from catch', subject);
+            return O.never();
+        })
+        .do(subject) // push messages into replay subject
+        .subscribe(function () {}, e => {}, _ => {
+            report('All done', subject);
+        });
+
+    function report (from, subject) {
+        console.log('reporting from', from);
+        subject.forEach(function (x) {
+            console.log(x);
+        });
+    }
+}
+
+handleConcatStream([
+    getcp('-1', `sleep 1`),
+    getcp('-2', 'sleep 1'),
     O.merge(
-        getcp(3, 'sleep'),
-        getcp(4, 'sleep 1')
-    ).catch(x => O.empty()),
+        getcp('---3', 'sleep 1'),
+        getcp('---4', 'sleep'),
+        O.concat(getcp('---5', 'sleep 1'), getcp('------6', 'sleep 1'), getcp('---------7', 'sleep 1')),
+        getcp('---8', 'sleep 1')
+    ),
     O.concat(
-        getcp(5, 'sleep 1'),
-        getcp(6, 'sleep 1')
+        getcp('-9', 'sleep 1'),
+        getcp('-10', 'sleep 1')
     )
-)
-.subscribe(x => {
-    console.log(x);
-}, e => {
-    // console.log('error', e);
-}, _ => {
-    console.log('ALL DONE');
-});
+]);
+
+
+// var noFailStream = [
+//     getcp('-1', `sleep 1`).catch(e => empty()),
+//     getcp('-2', 'sleep 1').catch(e => empty()),
+//     O.merge(
+//         getcp('---3', 'sleep 1').catch(e => empty()),
+//         getcp('---4', 'sleep 1').catch(e => empty()),
+//         O.concat(getcp('---5', 'sleep'), getcp('------6', 'sleep 1'), getcp('---------7', 'sleep 1')).catch(e => empty()),
+//         getcp('---8', 'sleep 1').catch(e => empty())
+//     ),
+//     O.concat(
+//         getcp('-9', 'sleep 1').catch(e => empty()),
+//         getcp('-10', 'sleep 1').catch(e => empty())
+//     )
+// ];
+//
+// const sub = Rx.Observable
+//     .concat(noFailStream)
+//     .subscribe(x => {
+//         console.log(x);
+//     }, e => {
+//         // console.log('error', e);
+//     }, _ => {
+//         console.log('ALL DONE');
+//     });
+
+
 
 // setTimeout(function () {
 // 	stream.dispose();
