168fdd4e7f2f569f82e9483086215c201c349b39
Shane Osbourne <shane.osbourne8@gmail.com>
2016-03-19 10:25:04 +0000

    Add parallel, series and no-fail stream examples

--- example.js
+++ example.js
@@ -7,10 +7,124 @@ var concat = O.concat;
 var merge = O.merge;
 var cp = require('child_process').spawn;
 
-var subject = new Rx.ReplaySubject(2000);
-var sub$ = subject.share();
+/**
+ * A top-level parallel stream will not exit if any siblings fail.
+ * eg:
+ *      1 fails, 2, 3... still run
+ *
+ * Note: Nested series groups will still fail
+ * eg:
+ *      task 5 is followed by 6 & 7 in series
+ *      task 5 fails -> 6 & 7 do not run, but siblings to
+ *      5 will still continue
+ * @type {*[]}
+ */
+var parallelStream = [
+    task('-1', `sleep 1`).catch(e => O.empty()),
+    task('-2', 'sleep 1').catch(e => O.empty()),
+    O.merge(
+        task('---3', 'sleep 1').catch(e => O.empty()),
+        task('---4', 'sleep 1').catch(e => O.empty()), // ERROR
+        O.concat(task('---5', 'sleep 1'), task('------6', 'sleep 1'), task('---------7', 'sleep 1')).catch(e => O.empty()),
+        task('---8', 'sleep 1').catch(e => O.empty())
+    ).catch(e => O.empty()),
+    O.concat(
+        task('-9', 'sleep 1'),
+        task('-10', 'sleep 1')
+    ).catch(e => O.empty())
+];
+// handleParallelStream(parallelStream);
 
-function getcp (num, cmd, fail) {
+/**
+ * The default, series stream will halt upon any failures.
+ * This bubbles up, so that if 5 fails, all siblings halt and the parent
+ * halts too.
+ * @type {*[]}
+ */
+var seriesStream = [
+    task('-1', `sleep 1`),
+    task('-2', 'sleep 1'),
+    O.merge(
+        task('---3', 'sleep 1'),
+        task('---4', 'sleep'),
+        O.concat(task('---5', 'sleep 1'), task('------6', 'sleep 1'), task('---------7', 'sleep 1')),
+        task('---8', 'sleep 1')
+    ),
+    O.concat(
+        task('-9', 'sleep 1'),
+        task('-10', 'sleep 1')
+    )
+];
+
+/**
+ * No fail stream still runs tasks in series, but a failure in any individual
+ * branch will not cause the parent/siblings to fail
+ * @type {*[]}
+ */
+var noFailStream = [
+    task('-1', `sleep`).catch(e => empty()),
+    task('-2', 'sleep 1').catch(e => empty()),
+    O.merge(
+        task('---3', 'sleep 1').catch(e => empty()),
+        task('---4', 'sleep 1').catch(e => empty()),
+        O.concat(task('---5', 'sleep 1'), task('------6', 'sleep 1'), task('---------7', 'sleep 1')).catch(e => empty()),
+        task('---8', 'sleep 1').catch(e => empty())
+    ),
+    O.concat(
+        task('-9', 'sleep 1').catch(e => empty()),
+        task('-10', 'sleep 1').catch(e => empty())
+    )
+];
+// handleConcatStream(noFailStream);
+
+/**
+ * Merge stream will never fail at the top level, so we need to capture
+ * output and look at it in the onCompleted callback
+ * @param items
+ */
+function handleParallelStream(items) {
+    var subject = new Rx.ReplaySubject(2000);
+    Rx.Observable.merge(items)
+        .subscribe(x => {
+            subject.onNext(x);
+        }, e => {
+            // console.log('er');
+            // console.log('error', e);
+        }, _ => {
+            subject.forEach(function (x) {
+                console.log(x)
+            })
+        });
+}
+
+/**
+ * Concat stream can fail at any point, so we need to
+ * catch when that error occurs
+ * @param items
+ */
+function handleConcatStream(items) {
+    var subject = new Rx.ReplaySubject(2000);
+    Rx.Observable.from(items)
+        .concatAll()
+        .catch(e => { // global error
+            console.log('Error', 'but got these dope messages before it errored!');
+            report('from catch', subject);
+            return O.never();
+        })
+        .do(subject) // push messages into replay subject
+        .subscribe(function () {}, e => {}, _ => {
+            report('All done', subject);
+        });
+
+    function report (from, subject) {
+        console.log('reporting from', from);
+        subject.forEach(function (x) {
+            console.log(x);
+        });
+    }
+}
+
+function task (num, cmd, fail) {
     const obs = Rx.Observable.create(function (observer) {
         console.log(num, '+ running');
         observer.onNext({type: 'start', id: num});
@@ -53,182 +167,6 @@ function getcp (num, cmd, fail) {
     return obs;
 }
 
-// handleMergeStream([
-//     getcp('-1', `sleep 1`).catch(e => O.empty()),
-//     getcp('-2', 'sleep 2').catch(e => O.empty()),
-//     O.merge(
-//         getcp('---3', 'sleep 1').catch(e => O.empty()),
-//         getcp('---4', 'sleep').catch(e => O.empty()),
-//         O.concat(getcp('---5', 'sleep 1'), getcp('------6', 'sleep 1'), getcp('---------7', 'sleep 1')).catch(e => O.empty()),
-//         getcp('---8', 'sleep 1').catch(e => O.empty())
-//     ).catch(e => O.empty()),
-//     O.concat(
-//         getcp('-9', 'sleep'),
-//         getcp('-10', 'sleep 1')
-//     ).catch(e => O.empty())
-// ]);
-
-/**
- * Merge stream will never fail at the top level, so we need to capture
- * output and look at it in the onCompleted callback
- * @param items
- */
-function handleMergeStream(items) {
-    Rx.Observable.merge(items)
-        .subscribe(x => {
-            subject.onNext(x);
-        }, e => {
-            // console.log('er');
-            // console.log('error', e);
-        }, _ => {
-            subject.forEach(function (x) {
-                console.log(x)
-            })
-        });
-}
-
-/**
- * Concat stream can fail at any point, so we need to
- * catch when that error occurs
- * @param items
- */
-function handleConcatStream(items) {
-    var subject = new Rx.ReplaySubject(2000);
-    Rx.Observable.from(items)
-        .concatAll()
-        .catch(e => { // global error
-            console.log('Error', 'but got these dope messages before it errored!');
-            report('from catch', subject);
-            return O.never();
-        })
-        .do(subject) // push messages into replay subject
-        .subscribe(function () {}, e => {}, _ => {
-            report('All done', subject);
-        });
-
-    function report (from, subject) {
-        console.log('reporting from', from);
-        subject.forEach(function (x) {
-            console.log(x);
-        });
-    }
-}
-
-handleConcatStream([
-    getcp('-1', `sleep 1`),
-    getcp('-2', 'sleep 1'),
-    O.merge(
-        getcp('---3', 'sleep 1'),
-        getcp('---4', 'sleep'),
-        O.concat(getcp('---5', 'sleep 1'), getcp('------6', 'sleep 1'), getcp('---------7', 'sleep 1')),
-        getcp('---8', 'sleep 1')
-    ),
-    O.concat(
-        getcp('-9', 'sleep 1'),
-        getcp('-10', 'sleep 1')
-    )
-]);
-
-
-// var noFailStream = [
-//     getcp('-1', `sleep 1`).catch(e => empty()),
-//     getcp('-2', 'sleep 1').catch(e => empty()),
-//     O.merge(
-//         getcp('---3', 'sleep 1').catch(e => empty()),
-//         getcp('---4', 'sleep 1').catch(e => empty()),
-//         O.concat(getcp('---5', 'sleep'), getcp('------6', 'sleep 1'), getcp('---------7', 'sleep 1')).catch(e => empty()),
-//         getcp('---8', 'sleep 1').catch(e => empty())
-//     ),
-//     O.concat(
-//         getcp('-9', 'sleep 1').catch(e => empty()),
-//         getcp('-10', 'sleep 1').catch(e => empty())
-//     )
-// ];
-//
-// const sub = Rx.Observable
-//     .concat(noFailStream)
-//     .subscribe(x => {
-//         console.log(x);
-//     }, e => {
-//         // console.log('error', e);
-//     }, _ => {
-//         console.log('ALL DONE');
-//     });
-
-
-
-// setTimeout(function () {
-// 	stream.dispose();
-// }, 500);
-// function getTask(name, time, schedule) {
-//     return O.create(observer => {
-//         O.timer(time, schedule || null)
-//             .subscribe(x => {
-//                 console.log('+', name);
-//                 observer.onNext(x);
-//             }, e => {}, _ => {
-//                 observer.onCompleted();
-//             });
-//     })
-// }
-//
-// function getError(name, time, scheduler) {
-//     return O.create(observer => {
-//         O.timer(time, scheduler || null)
-//             .subscribe(x => {
-//                 console.log('-', name);
-//                 observer.onError(new Error('Some proble'));
-//             });
-//     });
-// }
-//
-// //function fac(name) {
-// //    return O.create(obs => {
-// //        console.log('+ ', name);
-// //        let i = setTimeout(function () {
-// //        	obs.onNext(name);
-// //        	obs.onCompleted();
-// //        }, 500);
-// //        return () => {
-// //            if (i) {
-// //                console.log('- ', name);
-// //                clearTimeout(i);
-// //            }
-// //        }
-// //    });
-// //}
-// const onNext  = Rx.ReactiveTest.onNext;
-// const onError = Rx.ReactiveTest.onError;
-//
-// function getTasks (scheduler) {
-//     return [
-//         getTask('1', 100, scheduler),
-//         getTask('2', 100, scheduler),
-//         getError('3', 100, scheduler),
-//         O.concat(
-//             getTask('4', 100, scheduler),
-//             getTask('5', 100, scheduler),
-//             O.merge(
-//                 getTask('6', 100, scheduler),
-//                 getTask('7', 100, scheduler)
-//             )
-//         )
-//     ];
-// }
-//
-// ////const q = O.from(task1).concatAll();
-// ////
-// ////const sub = q.subscribe(x => {
-// ////        //console.log('+ ', x)
-// ////    }, e => {
-// ////        console.error(e)
-// ////    }, _ => {
-// ////        console.log('DONE');
-// ////    });
-// ////
-// ////setTimeout(function () {
-// ////	sub.dispose();
-// ////}, 600);
 // //
 // const TestScheduler = Rx.TestScheduler;
 // const just          = O.just;
