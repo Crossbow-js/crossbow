bfe015e0b2926a7992f9b3103bf15e9177f2aaf0
Shane Osbourne <shane.osbourne8@gmail.com>
2016-05-13 16:28:24 +0100

    Add command 'tasks' help when TaskNotFound Error

--- src/reporters/error.TaskNotFound.ts
+++ src/reporters/error.TaskNotFound.ts
@@ -2,5 +2,5 @@ import {Task} from "../task.resolve.d";
 import {TaskNotFoundError} from "../task.errors.d";
 
 module.exports = (error: TaskNotFoundError, task: Task) =>
-
-    `{red:-} {bold:Description}: {cyan:'${task.taskName}'} was not found.`;
+    `{red:-} {bold:Description}: {cyan:'${task.taskName}'} was not found.
+  To see a list of your available tasks, run {gray:$} {cyan.bold:crossbow tasks}`;
