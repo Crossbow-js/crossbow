ca099064d409428544655a8d811bf14b2846095c
Shane Osbourne <shane.osbourne8@gmail.com>
2016-05-17 18:01:52 +0100

    Failing test to show a situation where the lookup should occur as the name matches a top-level task definition

--- test/task.resolve.node-modules.js
+++ test/task.resolve.node-modules.js
@@ -22,12 +22,15 @@ describe('task.resolve from installed node_modules', function () {
         });
         assert.equal(runner.tasks.invalid[0].tasks[0].errors[0].type, TaskErrorTypes.TaskNotFound);
     });
-    // it('does not look at any files if the name matches a task definition', function () {
-    //     const runner = cli.getRunner(['js'], {
-    //         tasks: {
-    //             js: 'Krossbow-scass' // typo
-    //         }
-    //     });
-    //     assert.equal(runner.tasks.invalid[0].tasks[0].errors[0].type, TaskErrorTypes.TaskNotFound);
-    // });
+    it.only('does not look at any files if the name matches a task definition', function () {
+        const runner = cli.getRunner(['archy'], {
+            archy: {
+                js: 'crossbow-sass'
+            }
+        });
+
+        assert.equal(runner.tasks.valid[0].tasks[0].externalTasks[0].rawInput, 'crossbow-sass');
+        assert.equal(runner.tasks.valid[0].tasks[0].externalTasks[0].relative, 'node_modules/crossbow-sass/index.js');
+        // assert.equal(runner.tasks.invalid[0].tasks[0].errors[0].type, TaskErrorTypes.TaskNotFound);
+    });
 });
