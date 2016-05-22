9f22b7ae7e8c91b5032b448b2d79fded53590caa
Shane Osbourne <shane.osbourne8@gmail.com>
2016-05-22 19:00:07 +0100

    WIP adding module export detection

--- example.js
+++ example.js
@@ -1,4 +1,33 @@
-const path = require('path');
-process.chdir('../');
-console.log(path.resolve(process.cwd(), 'node_modules', 'crossbow-sass'))
-// console.log(require.resolve(path.resolve(process.cwd, 'node_modules', 'crossbow-sass')));
+var dtect = require('./p');
+
+var globa = dtect(`
+module.exports = function () {
+    console.log('Kittie');
+}
+
+exports = [function(){}];
+`);
+
+
+function getTopLevel (nodes) {
+    return nodes.filter(x => {
+        return x.name === 'exports' || x.name === 'module';
+    })
+}
+
+const top = getTopLevel(globa);
+
+function getModuleDotExports (nodes) {
+    return nodes.filter(x => {
+        if (x.name !== 'module') return false;
+        return x.nodes[0].parents.filter(function (node) {
+            return node.type === 'AssignmentExpression';
+        }).length;
+    });
+}
+
+
+console.log(getModuleDotExports(top));
+
+
+// console.log(globa[1].nodes[0].parents);
