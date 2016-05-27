7528af2040829a4d13926e83c9cb30a26619b8bb
Shane Osbourne <shane.osbourne8@gmail.com>
2016-05-27 20:21:32 +0100

    test: add failing test for before flags

--- test/command.watch.before.flag.js
+++ test/command.watch.before.flag.js
@@ -2,10 +2,9 @@ const assert      = require('chai').assert;
 const cli = require('../');
 
 describe('using tasks given as flags as before', function () {
-    it.skip('uses the before tasks given in cli.flags' , function () {
+    it.only('uses the before tasks given in cli.flags' , function () {
         const runner = cli.getWatcher(['default'], {
             watch: {
-                before: ['js'],
                 default: {
                     "*.css": ["sass", "js"],
                     "*.js":  ["js"]
@@ -17,6 +16,8 @@ describe('using tasks given as flags as before', function () {
             tasks: {
                 js: "test/fixtures/tasks/observable.js"
             }
+        }, {
+            before: ['js']
         });
 
         assert.equal(runner.before.tasks.valid.length, 1);
