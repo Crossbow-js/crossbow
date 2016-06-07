5c527a1b24060a6d51ed817fc9c187e652bc988b
Shane Osbourne <shane.osbourne8@gmail.com>
2016-06-07 23:53:24 +0100

    Prompt for init command if no tasks are found in the current directory

--- src/reporters/error.NoTasksAvailable.ts
+++ src/reporters/error.NoTasksAvailable.ts
@@ -2,5 +2,12 @@ import {NoTasksAvailableError} from "../task.utils";
 
 module.exports = (error: NoTasksAvailableError) => {
     return `{red:-} {bold:Description}: You didn't provide a task-name to run
-  and interactive run mode is also not available as there are no tasks defined`;
+  and interactive run mode is also not available as there are no tasks defined
+  
+  Perhaps you want to create a config file to use Crossbow in this directory?
+  
+  Try:
+  
+    $ crossbow {bold:init}
+  `;
 };
