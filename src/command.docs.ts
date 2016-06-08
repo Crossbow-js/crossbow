430c49babf2eb05d43a18b04c7d670117005615d
Shane Osbourne <shane.osbourne8@gmail.com>
2016-06-08 22:33:58 +0100

    Add todos

--- src/command.docs.ts
+++ src/command.docs.ts
@@ -17,7 +17,16 @@ export default function execute(trigger: CommandTrigger): any {
     const {input, config, reporter} = trigger;
     const resolved = resolveTasks(Object.keys(input.tasks), trigger);
 
+    /**
+     * Create the header for the markdown table
+     * @type {string|string[]}
+     */
     const header   = ['|Name|Description|', '|---|---|'];
+
+    /**
+     * Create the body for the table with taskname + description
+     * @type {string[]}
+     */
     const body     = resolved.valid.map((x: Task) => {
         const name = `|**\`${x.baseTaskName}\`**`;
         const desc = (function () {
@@ -29,33 +38,43 @@ export default function execute(trigger: CommandTrigger): any {
         return [name, desc].join('|');
     });
 
+    /**
+     * Join the lines with a \n for correct formatting in markdown
+     * @type {string}
+     */
     const markdown = header.concat(body).join('\n');
 
+    /**
+     * If config.handoff, just return the tasks + markdown string
+     * to skip any IO
+     */
     if (trigger.config.handoff) {
         return {tasks: resolved, markdown};
     }
 
+    /**
+     * If there were 0 tasks, exit with error
+     */
     if (resolved.all.length === 0) {
         reporter(ReportNames.NoTasksAvailable);
         return {tasks: resolved};
     }
 
+    /**
+     * If any tasks were invalid, refuse to generate docs
+     * and prompt to run tasks command (for the full error output)
+     */
     if (resolved.invalid.length) {
         reporter(ReportNames.InvalidTasksSimple);
         return {tasks: resolved};
     }
 
-    console.log(markdown);
-
-    // console.log(resolved.invalid);
-    //     config.verbose === LogLevel.Verbose
-    // ) {
-    //     reporter(ReportNames.TaskTree, resolved.all, config, 'Available tasks:');
-    // } else {
-    //     reporter(ReportNames.SimpleTaskList, getSimpleTaskList(resolved.valid), resolved.valid);
-    // }
-    //
-    // return {tasks: resolved};
+    // todo: 1 - look for readme.md files
+    // todo: 2 - look for comments in readme to signify start/end positions for docs
+    // todo: 3 - if start/end positions are not in the doc, append to end of file
+    // todo: 4 - allow --file flag to choose a different file (for the comment search)
+    // todo: 5 - allow --output flag to instead output to a brand new file
+    
     return 'shane';
 }
 
