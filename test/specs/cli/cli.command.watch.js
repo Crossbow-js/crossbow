387cba298cfe28d0b728fe4e6349f3e91f808270
Shane Osbourne <shane.osbourne8@gmail.com>
2016-10-12 13:53:00 +0100

    watch command interactive (fix test)

--- test/specs/cli/cli.command.watch.js
+++ test/specs/cli/cli.command.watch.js
@@ -1,9 +1,11 @@
-const exec = require('child_process').exec;
+const exec   = require('child_process').exec;
+const assert = require('chai').assert;
 
 describe("watch command", function () {
     it("Reports when no watchers available", function (done) {
-        exec('node dist/cb watch --cwd test', function (err) {
-            if (err) return done(err);
+        exec('node dist/cb watch --cwd test', function (err, stdout) {
+            assert.equal(err.code, 1);
+            assert.include(stdout, 'NoWatchersAvailable');
             done();
         });
     });
