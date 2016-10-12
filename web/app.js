e54a25136ddf40ea9650dd31773d08c66485e900
Shane Osbourne <shane.osbourne8@gmail.com>
2016-10-12 22:18:19 +0100

    hacking on UI stuff

---
+++ web/app.js
@@ -0,0 +1,134 @@
+(function (io, angular) {
+
+    angular
+        .module('crossbow', [])
+        .service('Tasks', function () {
+            var socket   = io();
+            var task$    = new Rx.BehaviorSubject([]);
+            socket.on('TopLevelTasks', function (_tasks) {
+                task$.onNext(_tasks);
+            });
+            const execReport$ = Rx.Observable.fromEvent(socket, 'execute-report');
+            const complete$   = execReport$.filter(x => x.data.type === 'Complete');
+            return {
+                task$,
+                execute: function (tasks) {
+                    const id = '01';
+                    return Rx.Observable
+                        .just(true)
+                        .do(x => {
+                            socket.emit('execute', {
+                                id,
+                                cli: {
+                                    input: ['run'].concat(tasks),
+                                    flags: {}
+                                }
+                            });
+                        })
+                        .flatMap(function () {
+                            return execReport$.filter(x => x.origin === id).takeUntil(complete$.filter(x => x.origin === id));
+                        }).map(x => x.data);
+                }
+            }
+        })
+        .directive('taskList', function () {
+            return {
+                template: `
+<div ng-show="ctrl.current.runnables.length">
+<ol>
+    <li ng-repeat="runnable in ctrl.current.runnables track by runnable.seqUID">
+        <pre>{{runnable.task.taskName}}</pre>
+        <span ng-show="runnable.stats.started && !runnable.stats.completed">started</span>
+        <span ng-show="runnable.stats.completed">done {{runnable.stats.duration/1000}}s</span>
+        <span style="color: red" ng-show="runnable.stats.errors.length">Failed {{runnable.stats.errors[0]}}</span>
+    </li>
+</ol>
+<div ng-show="ctrl.current.complete"><button ng-click="ctrl.reset()">Back to tasks</button></div>
+</div>
+<ul ng-show="!ctrl.current.runnables.length">
+    <li ng-repeat="task in ctrl.tasks track by $index">{{task}} 
+        <a href="" ng-click="ctrl.execute(task)">Run</a>
+    </li>
+</ul>
+`,
+                controllerAs: 'ctrl',
+                restrict: 'E',
+                controller: ['$timeout', '$scope', 'Tasks', function ($timeout, $scope, Tasks) {
+                    var ctrl   = this;
+                    ctrl.tasks = [];
+                    ctrl.current = {
+                        runnables: [],
+                        completed: [],
+                        errors:    [],
+                        complete:  false
+                    };
+
+                    ctrl.reset = function () {
+                        ctrl.current = {
+                            runnables: [],
+                            completed: [],
+                            errors:    [],
+                            complete:  false
+                        };
+                    };
+
+                    ctrl.applyStats = function (seqUID, stats) {
+                        ctrl.current.runnables
+                            .filter(runnable => runnable.seqUID === seqUID)[0]
+                            .stats = stats;
+                    };
+
+                    Tasks.task$.subscribe(x => {
+                        $timeout(function () {
+                            ctrl.tasks = x;
+                        });
+                    });
+
+                    ctrl.execute = function (task) {
+
+                        const runner = Tasks.execute(task);
+
+                        runner
+                            .do(x => console.log(x.type))
+                            .subscribe(x => {
+
+                                if (x.type === 'Setup') {
+                                    ctrl.current.runnables = collectRunnableTasks(x.data.sequence, []);
+                                    console.log(`Setup ${ctrl.current.runnables.length}`);
+                                }
+
+                                if (x.type === 'TaskReport') {
+                                    if (x.data.type === 'start') {
+                                        ctrl.applyStats(x.data.item.seqUID, x.data.stats);
+                                    }
+                                    if (x.data.type === 'end') {
+                                        ctrl.current.completed.push(x.data);
+                                        ctrl.applyStats(x.data.item.seqUID, x.data.stats);
+                                    }
+                                    if (x.data.type === 'error') {
+                                        ctrl.current.errors.push(x.data);
+                                        ctrl.applyStats(x.data.item.seqUID, x.data.stats);
+                                    }
+                                }
+
+                                if (x.type === 'Complete') {
+                                    ctrl.current.complete = true;
+                                }
+
+                                $scope.$digest();
+                            });
+                    }
+                }]
+            }
+        });
+
+    function collectRunnableTasks (items, initial) {
+        return items.reduce(function (acc, item) {
+            if (item.type === 'Task') {
+                return acc.concat(item);
+            }
+            return acc.concat(collectRunnableTasks(item.items, []));
+        }, initial);
+    }
+
+})(io, angular);
---
+++ web/server.js
@@ -0,0 +1,48 @@
+// Setup basic express server
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 4000;
+const cli            = require('../dist/index');
+const Rx             = require('rx');
+const outputObserver = new Rx.Subject();
+const signalObserver = new Rx.Subject();
+
+
+server.listen(port, function () {
+    console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname));
+
+// Chatroom
+
+// var numUsers = 0;
+//
+io.on('connection', function (socket) {
+    const prepared = cli.prepareInput({input: [], flags: {}}, null, outputObserver, signalObserver);
+    if (prepared.errors.length) {
+        console.log(prepared.errors);
+    }
+    const input = prepared.userInput.inputs[0].tasks;
+    socket.emit('TopLevelTasks', Object.keys(input));
+    socket.on('execute', function (incoming) {
+        const prepared = cli.prepareInput(incoming.cli, null, outputObserver, signalObserver);
+        if (prepared.errors.length) {
+            console.log(prepared.errors);
+        } else {
+            const out = cli.handleIncoming(prepared);
+            out.subscribe(x => {
+                if (x.type === 'TaskReport' && x.data.type === 'error') {
+                    const errors = x.data.stats.errors;
+                    x.data.stats.errors = x.data.stats.errors.map(x => {
+                        return x.toString();
+                    });
+                }
+                socket.emit('execute-report', {origin: incoming.id, data: x});
+            });
+        }
+    });
+});
